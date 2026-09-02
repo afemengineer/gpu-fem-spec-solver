275bc911e250634d58541a15c3191bfff45eb420
afemengineer <afemengineer@gmail.com>
2026-09-03 01:09:24 +0200

    M5 fix cached A1P1 benchmark include order

--- benchmarks/m5_cached_a1p1_dense_scratch_bench.cpp
+++ benchmarks/m5_cached_a1p1_dense_scratch_bench.cpp
@@ -4,6 +4,7 @@
 // LocalColumns hash accumulator until this candidate is validated.
 #include "recursive_sa_local_l2_helpers.inc"
 #include "recursive_sa_actual_a1_strength_local_helpers.inc"
+#include "m5_l2_dense_setup.hpp"
 #include "m5_fast_hierarchy_setup.hpp"
 #include "m5_materialized_a1_setup.hpp"
 #include "m5_parallel_actual_a1_setup.hpp"
@@ -13,6 +14,7 @@
 #include <chrono>
 #include <cmath>
 #include <cstddef>
+#include <cstdint>
 #include <iomanip>
 #include <iostream>
 #include <stdexcept>
