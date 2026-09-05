703867a4657d10c774bd63e911300de551556560
afemengineer <afemengineer@gmail.com>
2026-09-05 03:00:35 +0200

    M5 fix fine-context timing type typo

--- include/gfss/gpu_m5_fine_level.hpp
+++ include/gfss/gpu_m5_fine_level.hpp
@@ -50,7 +50,7 @@ struct GpuM5FineFullShellResult {
     std::vector<float> coarse_residual;
     std::vector<float> fine_correction_aos;
     GpuM5FineFullShellTiming median_timing;
-    GpuM5FullShellTiming best_timing;
+    GpuM5FineFullShellTiming best_timing;
     std::size_t smoother_degree{0};
     std::size_t transfer_smoothing_steps{0};
     std::size_t fine_operator_applies{0};
